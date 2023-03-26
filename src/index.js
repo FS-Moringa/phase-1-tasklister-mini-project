3ee5a0020f8bd7830b7f50b2a9a6a54b1e61bef8
Emmanuel Kipsang <emmqnuelkipsqng@gmail.com>
2023-03-26 11:47:40 -0700

    updated the tasklister

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.getElementById('create-task-form');
+  const taskList = document.getElementById('tasks');
+
+  form.addEventListener('submit', function (event) {
+    event.preventDefault();//this preventss the form from submitting and reloading the page..
+    const input = document.getElementById('new-task-description');
+    const description = input.value;
+    const li = document.createElement('li');
+    li.textContent = description;
+    taskList.appendChild(li);
+    input.value = '';
+  })
 });
